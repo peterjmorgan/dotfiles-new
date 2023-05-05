d58524f971f854ecfdfa2af90fac04075e447fdb
pmorgan <peterjmorgan@gmail.com>
2023-05-05 07:21:37 -0700

    fix init.vim for autorun

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,6 +2,19 @@ print("Welcome Pete! Let's write some code today :)")
 
 vim.g.mapleader = " "
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 require("packer").startup(function(use)
 	use { "wbthomason/packer.nvim" }
 	use { "ellisonleao/gruvbox.nvim" }
@@ -42,7 +55,10 @@ require("packer").startup(function(use)
 	use "jhlgns/naysayer88.vim",
 	use "terrortylor/nvim-comment",
 	use "CreaturePhil/vim-handmade-hero"
-}
+  }
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
 -- some
